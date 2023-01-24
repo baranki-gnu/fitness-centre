d33c619c10fa46493375e47907f4e043d40a2b13
baranki-gnu <98429714+baranki-gnu@users.noreply.github.com>
2023-01-25 02:14:02 +0300

    Соревнования и скрипт блока Абонементы

--- source/js/utils/tabs-switch.js
+++ source/js/utils/tabs-switch.js
@@ -1,70 +1,38 @@
-let tabsButtonsList = document.querySelector('[data-tabs-buttons]');
-let tabsButtons = Array.from(tabsButtonsList.querySelectorAll('[data-duration]'));
-
-function findActiveButton() {
-  let activeButton = tabsButtonsList.querySelector('.exercise-pass__duration-item--active button');
-  console.log(activeButton);
-
-  return activeButton;
-}
-
-function isButtonActive(button) {
-  if (button.dataset.duration === findActiveButton().dataset.duration) {
-    console.log('да');
-    return true;
-  }
-  console.log('нет');
-
-  return false;
-}
-
-function findActiveTab(dataInfo) {
-  console.log('дата ' + dataInfo);
-  let tabsList = document.querySelector('[data-tabs-content]');
-  //let string = 'data-tab=' + '\"' + dataInfo + '\"';
-  let activeTab = tabsList.querySelector('[data-tab=dataInfo]');
-  console.log('активный таб' + activeTab);
-
-  return activeTab;
-}
-
-
 function switchTab() {
-  tabsButtons.forEach((item) => {
-    item.addEventListener('click', () => {
-      console.log('клик');
 
-      if (isButtonActive(item)) {
+  let tabsButtons = document.querySelectorAll('.exercise-pass__duration-item button');
 
-        return;
-      }
+  if (tabsButtons) {
 
-      let activeButton = findActiveButton();
-      console.log(activeButton);
-      let activeData = activeButton.dataset.duration;
-      //let activeDataString = Object.prototype.toString.call(activeData);
-      //let activeDataString = activeData.toString();
-      //let activeData = activeButton.dataset;
-      console.log('данные активной кнопки' + activeData);
-      let activeTab = findActiveTab(activeData);
+    tabsButtons.forEach((item) => {
+      item.addEventListener('click', () => {
+        console.log('клик');
+
+        if (document.querySelector('.exercise-pass__duration-item--active')) {
+          document.querySelector('.exercise-pass__duration-item--active').classList.remove('exercise-pass__duration-item--active');
+        }
 
 
-      activeButton.classList.remove('exercise-pass__duration-item--active');
-      activeTab.classList.remove('tab-list__item--current');
-
-      let itemData = item.dataset.duration;
+        item.closest('.exercise-pass__duration-item').classList.add('exercise-pass__duration-item--active');
+        console.log(item);
 
 
-      //console.log(item.dataset.duration);
-
-      let itemTab = findActiveTab(itemData);
-
-      item.classList.add('exercise-pass__duration-item--active');
-      itemTab.classList.remove('tab-list__item--current');
+        if (document.querySelector('.tab-list__item--current')) {
+          document.querySelector('.tab-list__item--current').classList.remove('tab-list__item--current');
+        }
 
 
+        let targetDuration = item.dataset.duration;
+        console.log(targetDuration);
+        let targetList = document.querySelector(`.tab-list__item[data-tab="${targetDuration}"]`);
+
+        if (targetList) {
+          targetList.classList.add('tab-list__item--current');
+        }
+      });
     });
-  });
+  }
+
 }
 
 export {switchTab};
