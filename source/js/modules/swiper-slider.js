9c7e710d30f82405d048084036a78c87140ab191
baranki-gnu <98429714+baranki-gnu@users.noreply.github.com>
2023-01-27 03:32:36 +0300

    Дальнейшая работа по тренерам и преимуществам

--- source/js/modules/swiper-slider.js
+++ source/js/modules/swiper-slider.js
@@ -10,6 +10,26 @@ const swiper = new Swiper('.swiper', {
   slidesPerGroup: 4,
   freeMode: true,
 
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      slidesPerGroup: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      slidesPerGroup: 2,
+    },
+
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      slidesPerGroup: 4,
+    },
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
